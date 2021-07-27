7d8ceabca9a91927ee7888b27a8bca40db0a4828
betterchalk-blossom <blossom@betterchalk.com>
2021-07-27 22:38:51 +0000

    Changes

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,7 +1,7 @@
-import {Model, model, property} from '@loopback/repository';
+import {Entity, Model, model, property} from '@loopback/repository';
 
 @model()
-export class Todo extends Model {
+export class Todo extends Entity {
   @property({
     type: 'number',
     id: true,
